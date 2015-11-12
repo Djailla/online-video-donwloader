2c6f813cc4184200cc53bb80b7a5aedc13507698
Djailla <bastien.vallet@gmail.com>
2015-11-13 00:13:03 +0100

    Code cleanup and add thumbnail

--- com.djailla.online-video-downloader/build.sh
+++ com.djailla.online-video-downloader/build.sh
@@ -7,7 +7,6 @@ pip install youtube-dl
 ln -s /usr/local/bin/youtube-dl /usr/bin/
 
 mkdir -m 755 -p /opt/youtube
-# install -m 755 /home/source/app/main.py /opt/youtube
 cp -r /home/source/app/* /opt/youtube
 
 exit 0
