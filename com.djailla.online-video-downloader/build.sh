1d981beb50936c72a73eeeff73b2d6dd977d4cbf
rainbow <bastien.vallet@gmail.com>
2016-01-04 18:56:11 +0100

    Specify the version of components to embed in the app

--- com.djailla.online-video-downloader/build.sh
+++ com.djailla.online-video-downloader/build.sh
@@ -2,8 +2,8 @@
 
 install -m 755 /home/source/rc.local /etc
 
-pip install bottle
-pip install youtube-dl
+pip install bottle==0.12.9	
+pip install youtube-dl==2016.01.01
 ln -s /usr/local/bin/youtube-dl /usr/bin/
 
 mkdir -m 755 -p /opt/youtube
