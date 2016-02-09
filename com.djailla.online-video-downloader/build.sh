b454ed2ea0355079453b68c6b2d6ffde6891904f
Djailla <bastien.vallet@gmail.com>
2016-02-09 15:42:20 +0100

    Fix issue #2 by including locales to the app. Update version of youtube-dl to latest

--- com.djailla.online-video-downloader/build.sh
+++ com.djailla.online-video-downloader/build.sh
@@ -2,8 +2,11 @@
 
 install -m 755 /home/source/rc.local /etc
 
+apt-get update
+apt-get install -y -q locales
+
 pip install bottle==0.12.9
-pip install youtube-dl==2016.02.05.1
+pip install youtube-dl==2016.02.09
 ln -s /usr/local/bin/youtube-dl /usr/bin/
 
 mkdir -m 755 -p /opt/youtube
