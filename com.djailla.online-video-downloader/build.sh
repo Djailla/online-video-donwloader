22ca7f878d287902b55f159028f027c55b7ebf53
Djailla <bastien.vallet@gmail.com>
2016-05-04 15:36:05 +0200

    Use new version of youtube-dl

--- com.djailla.online-video-downloader/build.sh
+++ com.djailla.online-video-downloader/build.sh
@@ -6,7 +6,7 @@ apt-get update
 apt-get install -y -q locales
 
 pip install bottle==0.12.9
-pip install youtube-dl==2016.02.09
+pip install youtube-dl==2016.05.01
 ln -s /usr/local/bin/youtube-dl /usr/bin/
 
 mkdir -m 755 -p /opt/youtube
